20b4648037824aa941b16f313b54a0c76c460833
EmmaR-OSU <rastatter.6@osu.edu>
2015-10-09 10:44:58 -0400

    Added Esmeer function file

---
+++ includes/wc-pos-esmeer.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+Functions for Esmeer customizations to WooPOS
+*/
+
+function verify(){
+	$current_user = wp_get_current_user();
+	$servername = "localhost";
+    $username = "root";
+    $password = "root";
+	$dbname = "pos";
+	// Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+	$sql = "SELECT storename FROM username_store WHERE username = '" . $current_user->user_login . "'";
+	$result = $conn->query($sql);
+	$row = $result->fetch_row();
+	$conn->close();
+	
+	if( count($row)>=1){
+		return $row[0];
+	}
+	else{
+		return -1;
+	}
+}
