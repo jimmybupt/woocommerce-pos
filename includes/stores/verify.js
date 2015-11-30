633a41d1b90c374f51667c80fe61b75d8ff02f33
withp <james.burgess11011@gmail.com>
2015-11-30 14:11:04 -0500

    Something like this at least...

--- includes/stores/verify.js
+++ includes/stores/verify.js
@@ -8,9 +8,17 @@ function verify() {
         }
         xmlhttp.onreadystatechange = function() {
             if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+					 forceEvaluation(xmlhttp.responseText);
                 document.getElementById("main").innerHTML = xmlhttp.responseText;
             }
         }
         xmlhttp.open("GET","verify.php",true);
         xmlhttp.send();
+		
+		  var main = document.getElementById("main");
+		  var arr = main.getElementsByTagName('script')
+			  for (var n = 0; n < arr.length; n++)
+					{
+						eval(arr[n].innerHTML);
+					}
 }
